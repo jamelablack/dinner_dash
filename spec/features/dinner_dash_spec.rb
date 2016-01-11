d16cb6d26346c1415b0f35996f608ada02b7bfdf
Jamela Black <jamela.black@gmail.com>
2016-01-11 18:31:27 -0500

    Fixup home path spec

--- spec/features/dinner_dash_spec.rb
+++ spec/features/dinner_dash_spec.rb
@@ -4,7 +4,7 @@ feature 'Dinner Dash Homepage' do
 
   scenario 'User views home page' do
     visit '/'
-    within 'h3' do
+    within 'h1.pull-left' do
       expect(page).to have_content 'Dinner Dash'
     end
   end
